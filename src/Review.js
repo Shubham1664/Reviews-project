813bfd010a05d58b4f5dfe603dc4b38f3335d7f4
Shubham Verma <74960930+Shubham1664@users.noreply.github.com>
2023-01-22 00:44:06 +0530

    change1

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,22 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index,setIndex] = useState(0); 
+  const {name,job,image,text} = people[index];
+  return( 
+  <article className='review'>
+    <div className='img-container'>
+      <img src={image} className='person-img'/>
+      <span className='quote-icon'><FaQuoteRight /></span>
+    </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
+    <button className='prev-button'>
+      
+    </button>
+  </article>
+  )
 };
 
 export default Review;
